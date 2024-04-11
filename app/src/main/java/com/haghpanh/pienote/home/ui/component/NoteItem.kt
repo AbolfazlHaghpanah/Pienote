ad789db907a925170b033ab4eb5befa917ab8a36
Abolfazl Haghpanah <abolfazlhaghpanah77@gmail.com>
2024-04-11 19:13:43 +0330

    implement swipeHandler and use in home NoteItem.kt

--- app/src/main/java/com/haghpanh/pienote/home/ui/component/NoteItem.kt
+++ app/src/main/java/com/haghpanh/pienote/home/ui/component/NoteItem.kt
@@ -1,18 +1,14 @@
 package com.haghpanh.pienote.home.ui.component
 
-import android.content.Context
-import android.os.Vibrator
-import android.view.MotionEvent
 import androidx.compose.animation.AnimatedContent
 import androidx.compose.animation.animateColorAsState
+import androidx.compose.animation.core.Spring
+import androidx.compose.animation.core.animateFloatAsState
+import androidx.compose.animation.core.spring
 import androidx.compose.animation.fadeIn
 import androidx.compose.animation.fadeOut
 import androidx.compose.animation.togetherWith
 import androidx.compose.foundation.background
-import androidx.compose.foundation.gestures.Orientation
-import androidx.compose.foundation.gestures.detectDragGestures
-import androidx.compose.foundation.gestures.draggable
-import androidx.compose.foundation.gestures.rememberDraggableState
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxHeight
@@ -23,17 +19,17 @@ import androidx.compose.material.Text
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.rounded.Delete
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.derivedStateOf
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableFloatStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.input.pointer.pointerInput
-import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.draw.rotate
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
 import com.haghpanh.pienote.baseui.theme.PienoteTheme
+import com.haghpanh.pienote.baseui.utils.SwipeHandlerState.Swipe
+import com.haghpanh.pienote.baseui.utils.rememberSwipeHandlerState
+import com.haghpanh.pienote.baseui.utils.swipeHandler
 
 @Composable
 fun HomeNoteItem(
@@ -42,24 +38,25 @@ fun HomeNoteItem(
     modifier: Modifier = Modifier,
     onDelete: () -> Unit
 ) {
-    val context = LocalContext.current
-
-    var draggableDelta by remember { mutableFloatStateOf(0f) }
+    val swipeHandlerState = rememberSwipeHandlerState(threshold = 240f)
+    val rightSwipe: Swipe by remember {
+        derivedStateOf { swipeHandlerState.rightSwipe }
+    }
     val animatedItemColor by animateColorAsState(
-        targetValue = if (draggableDelta > 0f) PienoteTheme.colors.error else PienoteTheme.colors.surface,
+        targetValue = if (rightSwipe.isSwiped && rightSwipe.isOffsetAchieveThreshold) {
+            PienoteTheme.colors.error
+        } else if (rightSwipe.isSwiped) {
+            PienoteTheme.colors.secondary
+        } else {
+            PienoteTheme.colors.surface
+        },
         label = "change color on delete"
     )
-    val draggableState = rememberDraggableState { delta ->
-        draggableDelta += delta
-    }
-
-    LaunchedEffect(draggableDelta) {
-        if (draggableDelta > 240f) {
-            (context.getSystemService(Context.VIBRATOR_SERVICE) as Vibrator?)?.apply {
-                vibrate(100)
-            }
-        }
-    }
+    val animatedRotation by animateFloatAsState(
+        targetValue = if (rightSwipe.isOffsetAchieveThreshold) 0f else 360f,
+        animationSpec = spring(Spring.DampingRatioMediumBouncy),
+        label = "delete icon rotation Animation"
+    )
 
     Column(
         modifier = modifier
@@ -69,13 +66,15 @@ fun HomeNoteItem(
             )
             .fillMaxWidth()
             .aspectRatio(2.4f)
-            .draggable(
-                state = draggableState,
-                orientation = Orientation.Horizontal
+            .swipeHandler(
+                state = swipeHandlerState,
+                onSwipeRight = onDelete,
+                shouldVibrateOnAchieveThreshold = true
             )
+
     ) {
         AnimatedContent(
-            targetState = draggableDelta > 0f,
+            targetState = rightSwipe.isSwiped,
             label = "show delete Icon or item Text",
             transitionSpec = { fadeIn().togetherWith(fadeOut()) }
         ) {
@@ -84,6 +83,7 @@ fun HomeNoteItem(
                     modifier = Modifier
                         .padding(24.dp)
                         .fillMaxHeight()
+                        .rotate(animatedRotation)
                         .aspectRatio(1f),
                     imageVector = Icons.Rounded.Delete,
                     contentDescription = "delete"
