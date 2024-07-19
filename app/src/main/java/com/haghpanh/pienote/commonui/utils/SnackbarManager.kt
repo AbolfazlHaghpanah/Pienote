49d8a4e5a606feb200241b1a1c3966781583740b
Abolfazl Haghpanah <abolfazlhaghpanah77@gmail.com>
2024-07-20 00:23:56 +0330

    #21 fix bugs in collection snackbars

--- app/src/main/java/com/haghpanh/pienote/commonui/utils/SnackbarManager.kt
+++ app/src/main/java/com/haghpanh/pienote/commonui/utils/SnackbarManager.kt
@@ -1,64 +1,162 @@
 package com.haghpanh.pienote.commonui.utils
 
 import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.slideInVertically
+import androidx.compose.animation.slideOutHorizontally
+import androidx.compose.animation.slideOutVertically
+import androidx.compose.foundation.gestures.detectDragGestures
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.Stable
-import androidx.compose.runtime.State
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
-import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.input.pointer.pointerInput
 import com.haghpanh.pienote.commonui.component.PienoteSnackbar
-import kotlinx.coroutines.channels.BufferOverflow
 import kotlinx.coroutines.channels.Channel
 import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.collectLatest
 import kotlinx.coroutines.flow.receiveAsFlow
+import kotlin.math.abs
 
 @Stable
 class SnackbarManager {
-    private val currentMessage = Channel<SnackbarData?>(
-        onBufferOverflow = BufferOverflow.DROP_OLDEST
-    )
+    private val _currentMessage = Channel<SnackbarData?>(Channel.BUFFERED)
+    val currentMessage = _currentMessage.receiveAsFlow()
 
     suspend fun sendMessage(data: SnackbarData) {
-        currentMessage.send(data)
+        _currentMessage.send(data)
     }
 
     suspend fun dismiss() {
-        currentMessage.send(null)
+        _currentMessage.send(null)
     }
 
-    @Composable
-    fun collectMessageAsState(): State<SnackbarData?> =
-        currentMessage.receiveAsFlow().collectAsStateWithLifecycle(initialValue = null)
+    suspend fun sendError(
+        message: String,
+        action: (() -> Unit)? = null,
+        duration: SnackbarDuration = SnackbarDuration.Short
+    ) {
+        val snackbarData = SnackbarData(
+            message = message,
+            type = SnackbarTypes.Error,
+            duration = duration,
+            action = action?.let {
+                SnackbarAction(
+                    action = action,
+                    label = "Try Again"
+                )
+            }
+        )
+
+        sendMessage(snackbarData)
+    }
+
+    suspend fun sendWarning(
+        message: String,
+        action: (() -> Unit)? = null,
+        actionLabel: String? = null,
+        duration: SnackbarDuration = SnackbarDuration.Short
+    ) {
+        val snackbarData = SnackbarData(
+            message = message,
+            type = SnackbarTypes.Warning,
+            duration = duration,
+            action = if (actionLabel != null && action != null) {
+                SnackbarAction(
+                    action = action,
+                    label = actionLabel
+                )
+            } else {
+                null
+            }
+        )
+
+        sendMessage(snackbarData)
+    }
+
+    suspend fun sendSuccess(
+        message: String,
+        action: (() -> Unit)? = null,
+        actionLabel: String? = null,
+        duration: SnackbarDuration = SnackbarDuration.Short
+    ) {
+        val snackbarData = SnackbarData(
+            message = message,
+            type = SnackbarTypes.Warning,
+            duration = duration,
+            action = if (actionLabel != null && action != null) {
+                SnackbarAction(
+                    action = action,
+                    label = actionLabel
+                )
+            } else {
+                null
+            }
+        )
+
+        sendMessage(snackbarData)
+    }
 }
 
 @Composable
 fun PienoteSnackbarHost(manager: SnackbarManager) {
-    val currentSnackbarData by manager.collectMessageAsState()
-    var shouldShowSnackbar by remember {
-        mutableStateOf(currentSnackbarData != null)
-    }
+    var currentSnackbarData: SnackbarData? by remember { mutableStateOf(null) }
+    var shouldShowSnackbar by remember { mutableStateOf(false) }
 
-    LaunchedEffect(currentSnackbarData) {
-        shouldShowSnackbar = false
+    LaunchedEffect(Unit) {
+        manager.currentMessage.collectLatest { snackbarData ->
+            currentSnackbarData = snackbarData
+            shouldShowSnackbar = false
 
-        currentSnackbarData?.let { snackbarData ->
-            shouldShowSnackbar = true
-            val duration = calculateDurationInMillis(snackbarData)
+            snackbarData?.let { data ->
+                shouldShowSnackbar = true
+                val duration = calculateDurationInMillis(data)
 
-            duration?.let {
-                delay(duration)
-                shouldShowSnackbar = false
+                duration?.let {
+                    delay(duration)
+                    shouldShowSnackbar = false
+                }
             }
         }
     }
 
-    AnimatedVisibility(shouldShowSnackbar) {
+    var slideOutAnimationId by remember {
+        mutableStateOf(0)
+    }
+
+    AnimatedVisibility(
+        visible = shouldShowSnackbar,
+        enter = slideInVertically(initialOffsetY = { it }),
+        exit = when (slideOutAnimationId) {
+            1 -> slideOutHorizontally(targetOffsetX = { it })
+            2 -> slideOutHorizontally(targetOffsetX = { -it })
+            else -> slideOutVertically(targetOffsetY = { it })
+        }
+    ) {
         currentSnackbarData?.let { snackbarData ->
-            PienoteSnackbar(snackbarDate = snackbarData)
+            PienoteSnackbar(
+                modifier = Modifier
+                    .pointerInput(Unit) {
+                        detectDragGestures(
+                            onDrag = { _, dragAmount ->
+                                if (dragAmount.y > 0 && dragAmount.y > abs(dragAmount.x)) {
+                                    slideOutAnimationId = 0
+                                    shouldShowSnackbar = false
+                                } else if (dragAmount.x > 0) {
+                                    slideOutAnimationId = 1
+                                    shouldShowSnackbar = false
+                                } else if (dragAmount.x < 0) {
+                                    slideOutAnimationId = 2
+                                    shouldShowSnackbar = false
+                                }
+                            }
+                        )
+                    },
+                snackbarDate = snackbarData
+            )
         }
     }
 }
@@ -86,27 +184,28 @@ private fun calculateDurationInMillis(snackbarData: SnackbarData): Long? {
     return when (snackbarData.duration) {
         SnackbarDuration.Short -> 3000L
         SnackbarDuration.Long -> 5000L
-        SnackbarDuration.BasedOnMessage -> {
-            val message = snackbarData.message
-            val countOfWords =
-                if (message.isEmpty()) {
-                    0
-                } else {
-                    message.trim().split("\\s+".toRegex()).size
-                }
+        SnackbarDuration.BasedOnMessage -> snackbarData.calculateDurationBasedOnText()
+        SnackbarDuration.Infinite -> null
+    }
+}
 
-            (countOfWords * 500L).let {
-                if (snackbarData.action == null && it > 3000L) {
-                    it
-                } else if (snackbarData.action != null) {
-                    it + 3000L
-                } else {
-                    3000L
-                }
-            }
+private fun SnackbarData.calculateDurationBasedOnText(): Long {
+    val message = message
+    val countOfWords =
+        if (message.isEmpty()) {
+            0
+        } else {
+            message.trim().split("\\s+".toRegex()).size
         }
 
-        else -> null
+    return (countOfWords * 500L).let {
+        if (action == null && it > 3000L) {
+            it
+        } else if (action != null) {
+            it + 3000L
+        } else {
+            3000L
+        }
     }
 }
 
